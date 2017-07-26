242d2a36da144b9a223066835b046814ab04aafc
xyz1001 <zgzf1001@gmail.com>
2017-07-26 23:52:12 +0800

    完成系统更新和软件安装部分内容

--- linux/setup.sh
+++ linux/setup.sh
@@ -25,4 +25,9 @@ echo "  1.1 修改软件源\n请手动切换软件源" && read -n 1
 echo "  1.2 正在更新系统"
 sudo apt update && sudo apt upgrade
 
-
+# 2. 安装基础软件
+echo "2. 安装基础软件"
+echo "  2.1 安装编译环境"
+sudo apt install g++ gcc cmake autotools python3 python3-pip qt5-qmake qt5-default lua5.3 build-essential
+echo "  2.2 安装开发环境"
+sudo apt install qttools5-dev-tools git gdb 
