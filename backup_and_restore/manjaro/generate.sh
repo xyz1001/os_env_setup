e9f3ad23e983a2a0db4fd1c59dc9b84ee6bc8ed6
xyz1001 <zgzf1001@gmail.com>
2018-03-04 18:04:15 +0800

    调整软件包列表生成脚本

--- backup_and_restore/manjaro/generate.sh
+++ backup_and_restore/manjaro/generate.sh
@@ -1,5 +1,5 @@
-- 获取pacman软件列表
-comm -12 <(cat ~/.bash_history ~/.zsh_history| grep "sudo pacman -S " | grep -v grep | awk -F'-S ' '{print $NF}' | tr ' ' '\n' | sed '/^\s*$/d' | sort | uniq) <(pacman -Qenq | sort)
+# 获取pacman软件列表
+comm -12 <(cat ~/.bash_history ~/.zsh_history| grep "sudo pacman -S " | grep -v grep | awk -F'-S ' '{print $NF}' | tr ' ' '\n' | sed '/^\s*$/d' | sort | uniq) <(pacman -Qenq | sort) > ./pacman.lst
 
-- 获取yaourt软件列表
-pacman -Qemq
+# 获取yaourt软件列表
+pacman -Qemq > ./yaourt.lst
